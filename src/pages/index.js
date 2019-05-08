8b6d77636058c0af01a53d370ab35931e70c619c
js-tut <johnsmilga@gmail.com>
2019-05-08 15:09:18 -0700

    8-inline-css

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,5 +4,8 @@ import Layout from "../components/Layout"
 export default () => (
   <Layout>
     Hello world!<Link to="/blog/">blog page</Link>
+    <h1 style={{ fontSize: "20px", textTransform: "capitalize", color: "red" }}>
+      hello styling
+    </h1>
   </Layout>
 )
