7fef1e1b49c2b749cb36c093c0ae2e8a3a66f596
js-tut <johnsmilga@gmail.com>
2019-05-08 19:57:09 -0700

    14-project-main-css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,9 @@
 import React from "react"
-// import "./navbar.css"
-import styles from "./navbar.module.css"
-console.log(styles)
 
 const Navbar = () => {
   return (
-    <div className={styles.navbar}>
+    <div>
       <h1>i'm navbar</h1>
-      <div className={styles.links}>i'm navbar link</div>
     </div>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,14 +3,6 @@ import { Link } from "gatsby"
 import Layout from "../components/Layout"
 export default () => (
   <Layout>
-    Hello world!<Link to="/blog/">blog page</Link>
-    <h1>hello styling</h1>
-    <div className="container">
-      <div className="row">
-        <div className="col-4">hello</div>
-        <div className="col-4">hello</div>
-        <div className="col-4">hello</div>
-      </div>
-    </div>
+    <div>hello from backroads</div>
   </Layout>
 )
