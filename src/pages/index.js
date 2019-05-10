5231e3550b26d4ca3cd33d5e5781747162eea6b9
js-tut <johnsmilga@gmail.com>
2019-05-09 20:33:02 -0700

    17-simple-hero

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
 import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/Layout"
-import { FaAdobe } from "react-icons/fa"
+import SimleHero from "../components/SimpleHero"
 export default () => (
   <Layout>
-    <div>hello from backroads</div>
+    <SimleHero />
   </Layout>
 )
