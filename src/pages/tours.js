27110b1b3fc24305c4d0d7d86a1a549e87c24b3c
js-tut <johnsmilga@gmail.com>
2019-05-12 16:21:12 -0700

    23-styled-components-basics

--- src/pages/tours.js
+++ src/pages/tours.js
@@ -1,7 +1,15 @@
 import React, { Component } from "react"
 import Layout from "../components/Layout"
+import Button from "../examples/Button"
 export default class tours extends Component {
   render() {
-    return <Layout>hello from tours page!!!!!</Layout>
+    return (
+      <Layout>
+        hello from tours page!!!!!
+        <div>
+          <Button>hello from styled</Button>
+        </div>
+      </Layout>
+    )
   }
 }
