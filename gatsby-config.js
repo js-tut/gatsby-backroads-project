d1797cdc939fb4430b0c3b57f1e3366815873d66
js-tut <johnsmilga@gmail.com>
2019-05-14 15:45:48 -0700

    35-site-meta-data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,10 @@
 module.exports = {
+  siteMetadata: {
+    title: "BackRoads",
+    description:
+      "Explore awesome worldwide tours & discover what makes each of them unique. Forget your daily routine & say yes to adventure",
+    author: "@johndoe",
+    data: ["item1", "item2"],
+  },
   plugins: [`gatsby-plugin-sass`, `gatsby-plugin-styled-components`],
 }
