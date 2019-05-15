70d29bda5e1e4c044569f45fd9ac36604168bdae
js-tut <johnsmilga@gmail.com>
2019-05-14 18:12:12 -0700

    38-useStaticQuery

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,10 @@ module.exports = {
     description:
       "Explore awesome worldwide tours & discover what makes each of them unique. Forget your daily routine & say yes to adventure",
     author: "@johndoe",
-    data: ["item1", "item2"],
+    data: {
+      name: "name exmaple",
+      age: 24,
+    },
   },
   plugins: [`gatsby-plugin-sass`, `gatsby-plugin-styled-components`],
 }
--- src/pages/tours.js
+++ src/pages/tours.js
@@ -1,7 +1,13 @@
 import React, { Component } from "react"
 import Layout from "../components/Layout"
+import Header from "../examples/Header"
 export default class tours extends Component {
   render() {
-    return <Layout>hello from tours page!!!!!</Layout>
+    return (
+      <Layout>
+        hello from tours page!!!!!
+        <Header />
+      </Layout>
+    )
   }
 }
