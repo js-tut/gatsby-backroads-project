ed03490abd3f0db4b911f92ddef9e785aaec29fb
js-tut <johnsmilga@gmail.com>
2019-05-14 21:50:57 -0700

    43-gatsby-source-filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,5 +9,15 @@ module.exports = {
       age: 24,
     },
   },
-  plugins: [`gatsby-plugin-sass`, `gatsby-plugin-styled-components`],
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      },
+    },
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-styled-components`,
+  ],
 }
