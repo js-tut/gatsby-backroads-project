4ad811159d22a076c62b1674ec279afdddd73ebf
js-tut <johnsmilga@gmail.com>
2019-05-14 20:55:02 -0700

    42-page-query

--- src/pages/tours.js
+++ src/pages/tours.js
@@ -1,13 +1,8 @@
 import React, { Component } from "react"
 import Layout from "../components/Layout"
-import Header from "../examples/RegularHeader"
+
 export default class tours extends Component {
   render() {
-    return (
-      <Layout>
-        hello from tours page!!!!!
-        <Header />
-      </Layout>
-    )
+    return <Layout>hello from tours page!!!!!</Layout>
   }
 }
