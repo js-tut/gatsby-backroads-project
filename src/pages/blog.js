94d3fae7fe3471eeb7ed09075e57672d3f7e02de
js-tut <johnsmilga@gmail.com>
2019-05-14 22:20:52 -0700

    42-page-query

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,13 +1,28 @@
 import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/Layout"
-
-const blog = () => {
+import { graphql } from "gatsby"
+const blog = ({ data }) => {
   return (
     <Layout>
       hello from blog page<Link to="/">back home</Link>
+      <h1>title : {data.site.siteMetadata.title}</h1>
     </Layout>
   )
 }
+export const query = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+        description
+        author
+        data {
+          age
+        }
+      }
+    }
+  }
+`
 
 export default blog
