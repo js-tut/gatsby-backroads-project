0b289e0510f3dbd489412b434fdf84cfd3ef8669
js-tut <johnsmilga@gmail.com>
2019-05-29 13:13:05 -0700

    103-seo-setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,12 +7,12 @@ module.exports = {
     description:
       "Explore awesome worldwide tours & discover what makes each of them unique. Forget your daily routine & say yes to adventure",
     author: "@johndoe",
-    data: {
-      name: "john",
-      age: 24,
-    },
+    twitterUsername: "@john_smilga",
+    image: "defaultBcg.jpeg",
+    siteUrl: "https://gatsby-backroad-project.netlify.com",
   },
   plugins: [
+    `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
